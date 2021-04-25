ef797cfc0dccb91c779bb5ba9b8804e2717e2a79
unknown <amienkowski@gmail.com>
2021-04-25 17:30:43 +0200

    First commit

---
+++ src/main/java/com/exercise/smart4viation/flightservice/Smart4viationFlightServiceApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.smart4viation.flightservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Smart4viationFlightServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Smart4viationFlightServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exercise/smart4viation/flightservice/Smart4viationFlightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.smart4viation.flightservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Smart4viationFlightServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
