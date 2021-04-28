bde038132fd333a859ee8c71dd0e4f7102f3b099
unknown <amienkowski@gmail.com>
2021-04-28 11:49:13 +0200

    Fixed contextLoads() test error - excluded MainMenu.class from scanning range.

--- src/test/java/com/exercise/smart4viation/flightservice/Smart4viationFlightServiceApplicationTests.java
+++ src/test/java/com/exercise/smart4viation/flightservice/Smart4viationFlightServiceApplicationTests.java
@@ -1,11 +1,21 @@
 package com.exercise.smart4viation.flightservice;
 
+import com.exercise.smart4viation.flightservice.data.JsonReader;
+import com.exercise.smart4viation.flightservice.data.WeightCalc;
+import com.exercise.smart4viation.flightservice.domain.CargoEntity;
+import com.exercise.smart4viation.flightservice.domain.CargoUnit;
+import com.exercise.smart4viation.flightservice.domain.FlightEntity;
+import com.exercise.smart4viation.flightservice.ui.ChoiceValidator;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@SpringBootTest(classes = {JsonReader.class,
+        WeightCalc.class,
+        CargoEntity.class,
+        CargoUnit.class,
+        FlightEntity.class,
+        ChoiceValidator.class,
+        Functionalities.class})
 class Smart4viationFlightServiceApplicationTests {
     @Test
     void contextLoads() {
